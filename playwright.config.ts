048088c05f31a19c3b2fe66558da1ff8a04392c4
Santiago Parra <sparra@clarasight.com>
2025-03-19 00:50:38 -0500

    chore: improve slack reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   testDir: './tests',
@@ -7,7 +10,18 @@ export default defineConfig({
     timeout: 15 * 1000,
   },
   fullyParallel: true,
-  reporter: [['list'], ['allure-playwright']],
+  reporter: [
+    ['list'],
+    ['allure-playwright'],
+    [
+      './node_modules/playwright-slack-report/dist/src/SlackReporter.js',
+      {
+        channels: ['qa-ui-automation'],
+        sendResults: 'always',
+        slackWebHookUrl: process.env.SLACK_WEBHOOK_URL,
+      },
+    ],
+  ],
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
